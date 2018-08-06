6e00fa53e5e045c8dbe4f2d4bbcad6b27fb0c19d
Jose Bujalance <joseanbu23@gmail.com>
2018-08-06 23:05:12 +0200

    Ensure capacity method

--- src/text_steganography.py
+++ src/text_steganography.py
@@ -9,10 +9,15 @@ class TextSteganography:
     The texts are converted to ASCII binaries (each character takes 7 bits) for capacity purposes.
     """
 
+    __LENGTH_TAG = cu.str2bin("SIZE")
+    __START_TAG = cu.str2bin("START")
+    __LENGTH_BITS = 32
+    # TODO remove this buggy delimiter and use tags
+    __DELIMITER = "1010101010101010"
+
     def __init__(self, input_filename):
         self.image = Image.open(input_filename).convert("RGB")
         self.pixels = self.image.load()
-        self.__delimiter = "1010101010101010"
 
     def hide_message(self, message, output):
         """
@@ -23,7 +28,7 @@ class TextSteganography:
         """
         new_image = Image.new(self.image.mode, self.image.size)
         new_pixels = new_image.load()
-        binary_message = cu.str2bin(message) + self.__delimiter
+        binary_message = cu.str2bin(message) + self.__DELIMITER
         msg_index = AutoIncrementInt(0)
         for x in range(self.image.size[0]):
             for y in range(self.image.size[1]):
@@ -45,7 +50,21 @@ class TextSteganography:
                 rgb_tuple = self.pixels[x, y]
                 for channel in rgb_tuple:
                     binary_msg += cu.get_lsb(cu.int2bin(channel))
-                    if binary_msg[-len(self.__delimiter):] == self.__delimiter:
-                        return cu.bin2str(binary_msg[:-len(self.__delimiter)])
+                    if binary_msg[-len(self.__DELIMITER):] == self.__DELIMITER:
+                        return cu.bin2str(binary_msg[:-len(self.__DELIMITER)])
         print("No message found !")
         return None
+
+    def __ensure_capacity(self, message):
+        """
+        Makes sure that there is enough available bits in the image to store the given message.
+        :param message: The message to store in the image.
+        :return: True if there is enough capacity, False otherwise
+        """
+        height, width = self.image.size
+        # TODO The last '1' should be later replaced by the number of LSB bits used per color chanel
+        available_bits = height * width * 3 * 1
+        # In ASCII, each character is encoded in 7 bits
+        message_size = len(message) * 7
+        boilerplate_size = len(self.__LENGTH_TAG) + self.__LENGTH_BITS + len(self.__START_TAG)
+        return available_bits >= message_size + boilerplate_size
